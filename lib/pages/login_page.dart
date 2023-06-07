2c46a195a370ff8b06c6bbcfb4cc2cff769283cb
Gruheeth <gruheeth.288@gmail.com>
2023-06-07 18:18:36 +0530

    Add username and password textfields

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,10 +1,67 @@
+import 'package:auth_ui/components/my_textfield.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
+  LoginPage({super.key});
+
+  // text editing controllers
+  final usernameController = TextEditingController();
+  final passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.grey.shade300,
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+              const SizedBox(height: 50),
+              // logo
+              const Icon(
+                Icons.lock,
+                size: 100,
+              ),
+              const SizedBox(height: 50),
+
+              //welcome back, you've been missed
+              Text(
+                'Welcome back, you\'ve been missed!',
+                style: TextStyle(
+                  color: Colors.grey.shade700,
+                  fontSize: 16,
+                ),
+              ),
+              const SizedBox(height: 25),
+
+              // username textfield
+              MyTextField(
+                controller: usernameController,
+                hintText: 'Username',
+                obscureText: false,
+              ),
+              const SizedBox(height: 10),
+
+              // password textfield
+              MyTextField(
+                controller: passwordController,
+                hintText: 'Password',
+                obscureText: true,
+              ),
+
+              // forgot password?
+
+              //sign in button
+
+              // or continue with
+
+              // google or apple sign in buttons
+
+              // not a member? register now
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
