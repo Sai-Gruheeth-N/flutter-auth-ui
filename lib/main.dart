50cc8c10f3446e607a78e98a63b9d76a566f6b7e
Gruheeth <gruheeth.288@gmail.com>
2023-06-07 18:02:05 +0530

    Create login page

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:auth_ui/pages/login_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(),
+      home: LoginPage(),
     );
   }
 }
